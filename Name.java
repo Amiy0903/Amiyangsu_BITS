87983adf6ba643ea2ba2ff5abdd1042482108f45
Amiy0903 <57384103+Amiy0903@users.noreply.github.com>
2020-06-27 00:38:49 +0530

    3rd commit

---
+++ Name.java
@@ -0,0 +1,12 @@
+class Name 
+{ 
+    // Your program begins with a call to main(). 
+    // Prints "Hello, World" to the terminal window. 
+    public static void main(String args[]) 
+    { 
+        for(i=0; i<=5; i++)
+	{
+	    System.out.println("Amiyangsu kar"+i);
+	 }
+    } 
+} 
